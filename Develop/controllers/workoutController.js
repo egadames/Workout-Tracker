2fc1b55eef7ba7866e2a80bfc0ee46f185b66b8c
Kire Semada <egadames990@gmail.com>
2020-05-19 21:06:27 -0700

    updated functions

--- Develop/controllers/workoutController.js
+++ Develop/controllers/workoutController.js
@@ -12,12 +12,33 @@ module.exports = {
         }
     },
     addExercise: async (req, res) => {
-
+        const { id } = req.params;
+        const { type, name, duration, distance, weight, reps, sets } = req.body;
+        try {
+            const newWorkout = await Workout.findByIdAndUpdate( id, {type, name, duration, distance, weight, reps, sets},{new: true,  runValidators: true});
+            return res.json(newWorkouts);
+        } catch (e) {
+            return res.status(403).json({ e });
+        }
     },
     createWorkout: async (req, res) => {
-
+        const { id } = req.params;
+        const { type, name, duration, distance, weight, reps, sets } = req.body;
+        try {
+            const newWorkout = await new Workout({ id, type, name, duration, distance, weight, reps, sets}).save();
+            return res.json(newWorkouts);
+        } catch (e) {
+            return res.status(403).json({ e });
+        }
     },
     getWorkoutsInRange: async (req, res) => {
-
+        try {
+            const rangeWorkouts = await Workout.find({}).limit(7);
+            console.log(rangeWorkouts)
+            return res.json(rangeWorkouts);
+        } catch (e) {
+            console.log(e)
+            return res.status(403).json(e);
+        }
     },
 };
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,21 +1,77 @@
-const { Schema, model } = require('mongoose');
-//
+// const { Schema, model } = require('mongoose');
+// //
 
-const WorkoutSchema = new Schema({
-    exercises: [{
-        type:{type: String},
-        name: {type: String},
-        duration: {type: Number},
-        weight: {type: Number},
-        reps: {type: Number},
-        sets: {type: Number},
-        distance: {type: Number},
-    }],
+// const WorkoutSchema = new Schema({
+//     exercises: [{
+//         type:{type: String},
+//         name: {type: String},
+//         duration: {type: Number},
+//         weight: {type: Number},
+//         reps: {type: Number},
+//         sets: {type: Number},
+//         distance: {type: Number},
+//     }],
+//     day: {
+//         type: Date,
+//         default: Date.now()
+//     },
+// });
+
+
+// module.exports = model('Workout', WorkoutSchema);
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: Date.now()
+      type: Date,
+      default: () => new Date()
     },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
+    }
+  }
+);
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
-
-
-module.exports = model('Workout', WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
